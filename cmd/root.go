033e2977866df270d1368003e5b218d8d2b2d237
paniccaaa <semaadamenko1@gmail.com>
2025-10-18 10:52:54 +0300

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2025 Semen Adamenko <semaadamenko1@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use: "tsunami",
+	// TODO: Add a description of the command help
+	Short: "Tsunami is a tool for load testing",
+	Long:  "Tsunami is a tool for load testing",
+	// TODO: Add a run function
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, World!")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tsunami.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 Semen Adamenko <semaadamenko1@gmail.com>
+*/
+package main
+
+import "github.com/paniccaaa/tsunami/cmd"
+
+func main() {
+	cmd.Execute()
+}
